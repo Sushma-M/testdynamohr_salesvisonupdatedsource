ca704bdf5cb48c2ea3f91ee1fbe2b4af2c2955e4
root <root@f66e21dbdfc2>
2016-05-31 10:00:27 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,10 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "testdynamohr",
+  "homePage" : "Main",
+  "name" : "testdynamohr",
+  "platformType" : "WEB",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
